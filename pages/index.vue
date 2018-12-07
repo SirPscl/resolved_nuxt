2d019f51d2a4ee5fb15ef111f177c2eb2e00d3b9
Pascal Huber <pascal.huber@resolved.ch>
2018-12-07 20:21:13 +0100

    update schema.org

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     itemtype="http://schema.org/Person">
     <meta
       itemprop="birthPlace"
-      content="Arbon">
+      content="St.Gallen">
     <meta
       itemprop="birthDate"
       content="1992-09-09">
@@ -16,14 +16,31 @@
       itemprop="alumniOf"
       content="ZHAW School of Engineering">
     <meta
+      itemprop="affiliation"
+      content="ETH Zürich">
+    <meta
       itemprop="homeLocation"
       content="Dübendorf">
+    <meta
+      itemprop="homeLocation"
+      content="Arbon">
+    <meta
+      itemprop="knowsAbout"
+      content="IT Software Computers Development">
     <div class="centered">
       <div class="site-title">
         <h1 itemprop="name">Pascal Huber</h1>
       </div>
       <div class="site-description">
-        <h2>I am a software <span class="highlight-text">developer</span>, computer science <span class="highlight-text">student</span> and <span class="highlight-text">co-founder</span> of QuickShift LLC.</h2>
+        <h2 itemprop="description">
+          I am a software
+          <span class="highlight-text">developer</span>,
+          computer science
+          <span class="highlight-text">student</span>
+          and <span class="highlight-text">co-founder</span> of
+          <a
+            href="https://quickshift.ch"
+            target="_blank">QuickShift</a>.</h2>
       </div>
     </div>
     <div class="site-links">
